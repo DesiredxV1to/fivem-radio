a4ed7a21dbb438b6ad5bd9933f7408e8134b281f
TheRaccoon <robert@theraccoon.dev>
2023-05-28 19:51:02 -0500

    Update fxmanifest.lua

--- radio/fxmanifest.lua
+++ radio/fxmanifest.lua
@@ -5,7 +5,7 @@ game 'gta5'
 -- Resource Info
 author 'Hellslicer'
 description 'This resource allows you to integrate your own radios in place of the original radios'
-version '2.0.0'
+version '2.1.0'
 
 -- Example custom radios
 -- Supersede RADIO_02_POP with custom radio
