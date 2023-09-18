a9d0f393825625ce8af2c3131ea0c3f73cbb499f
SincerelyVito <robert@theraccoon.dev>
2023-09-18 03:36:05 -0500

    Update radioScript.js
    Error Handling: You've added error handling for the fetch request, which is great. Consider adding more detailed error messages or handling other potential issues.
    
    Wrapped the entire script in an IIFE (Immediately Invoked Function Expression) to avoid polluting the global namespace.
    
     wrapping the entire script in an IIFE (Immediately Invoked Function Expression) to avoid polluting the global namespace.

--- radio/radioScript.js
+++ radio/radioScript.js
@@ -1,18 +1,25 @@
-let customRadios;
+(function() {
+    const customRadios = [];
 
-// Rest of the JavaScript code...
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://radio/radio:ready", { method: "POST", body: "{}" })
-        .catch(err => console.error("Error fetching radio: ", err));
-
-    window.addEventListener("message", (event) => {
-        const item = event.data;
-
-        switch (item.type) {
-            // Rest of the switch-case...
-            default:
-                console.error(`Unhandled message type: ${item.type}`);
-        }
+    document.addEventListener("DOMContentLoaded", () => {
+        initializeRadio();
+        setupMessageListener();
     });
-});
+
+    function initializeRadio() {
+        fetch("http://radio/radio:ready", { method: "POST", body: "{}" })
+            .catch(err => console.error("Error initializing radio: ", err));
+    }
+
+    function setupMessageListener() {
+        window.addEventListener("message", (event) => {
+            const messageData = event.data;
+
+            switch (messageData.type) {
+                // Rest of the switch-case...
+                default:
+                    console.error(`Unhandled message type: ${messageData.type}`);
+            }
+        });
+    }
+})();
